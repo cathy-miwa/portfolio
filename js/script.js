c29f61210617cb3deb2f7c1ca3a9acb83e6267e7
cathy-miwa <45130648+cathy-miwa@users.noreply.github.com>
2020-01-08 21:33:37 +0900

    沢山変更した、fix第一弾

--- js/script.js
+++ js/script.js
@@ -1319,3 +1319,16 @@ function scrollBlocker(flag){
     scrollBlockerFlag = false;
   }
 }
+
+
+//header矢印
+$(function(){
+	$('a[href^="#"]').click(function(){
+		var speed = 800;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
